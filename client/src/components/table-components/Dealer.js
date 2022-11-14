3bf0aea1430589bebecc477f0e45b96e9073fb72
Daniel Cavalcante <danielcavalcante@protonmail.com>
2022-11-14 20:55:20 -0300

    Added a timer functionality.

--- client/src/components/table-components/Dealer.js
+++ client/src/components/table-components/Dealer.js
@@ -1,19 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import convertSecondstoTime from "../../utils/timeConversor.js";
 import "./styles/Dealer.css";
 
-function Dealer() {
-  const [issue, setIssue] = React.useState(
-    "Click/Tap here to set table's issue..."
-  );
-  const [timer, setTimer] = React.useState("00:00:00");
+export default function Dealer() {
+  const [issue, setIssue] = useState("Click/Tap here to set table's issue...");
+  const [timer, setTimer] = useState(3600 * 13 - 1);
+
+  useEffect(() => {
+    const timerID = setInterval(() => setTimer(timer - 1), 1000);
+    return function cleanup() {
+      clearInterval(timerID);
+    };
+  }, [timer]);
+
   return (
     <div>
       <h3>{issue}</h3>
       <div>
-        <strong>Time left</strong> {timer}
+        <strong>Time left</strong> {convertSecondstoTime(timer)}
       </div>
     </div>
   );
 }
-
-export default Dealer;
---
+++ client/src/utils/timeConversor.js
@@ -0,0 +1,15 @@
+export default function convertSecondstoTime(timeInSeconds) {
+  const dateObj = new Date(timeInSeconds * 1000);
+  const hours = dateObj.getUTCHours();
+  const minutes = dateObj.getUTCMinutes();
+  const seconds = dateObj.getSeconds();
+
+  const timeString =
+    hours.toString().padStart(2, "0") +
+    ":" +
+    minutes.toString().padStart(2, "0") +
+    ":" +
+    seconds.toString().padStart(2, "0");
+
+  return timeString;
+}
